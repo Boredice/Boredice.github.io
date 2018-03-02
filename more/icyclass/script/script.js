e793ce776595f3b36dd9a86be0951fabf12290f3
Boredice <2419029544@qq.com>
2018-03-02 21:04:51 +0800

    改名更新

--- more/icyclass/script/script.js
+++ more/icyclass/script/script.js
@@ -115,16 +115,23 @@ for (var i = 0; i < seats.length; i++)
 		"LikeCount\">0</span><button class=\"unlikeBtn\" onClick=\"unlikeStudent('" + seats[i] + "')\"><i class=\"fa fa-thumbs-o-down\"></i></button></p></div>";
 	}
 }
+console.log("成功添加" + names.length + "位学生");
+document.getElementsByClassName("classIndexSelect")[0].onchange = function() {
+	document.getElementById("shareContainer").hidden = true;
+	share();
+};
 //赞
 function likeStudent(name)
 {
 	document.getElementById(name + "LikeCount").innerHTML = parseInt(document.getElementById(name + "LikeCount").innerHTML) + 1;
+	console.log("你给" + name + "点了一个赞");
 	sortTopList();
 }
 //踩
 function unlikeStudent(name)
 {
 	document.getElementById(name + "LikeCount").innerHTML = parseInt(document.getElementById(name + "LikeCount").innerHTML) - 1;
+	console.log("你给" + name + "点了一个踩");
 	sortTopList();
 }
 //排行
@@ -148,60 +155,94 @@ function sortTopList()
 		}
 	});
 	var topListStr = "";
+	var firstTopList = "";
 	for (var i = 0; i < topList.length; i++)
 	{
 		topList[i] = topList[i].substring(0, topList[i].indexOf(" "));
 		var name = topList[i];
+		if (i === 0)
+		{
+			firstTopList = name;
+		}
 		topList[i] = (i + 1) + " " + topList[i];
 		topList[i] += " (" + document.getElementById(name + "LikeCount").innerHTML + ")";
 		topListStr += i === topList.length - 1 ? topList[i] : topList[i] + "<br>";
 	}
 	document.getElementById("topList").innerHTML = topListStr;
+	console.log("成功排序了排行榜，第一名为" + firstTopList);
 }
 sortTopList();
+var remark = "";
 //分享
 function share()
 {
+	var shareContainer = document.getElementById("shareContainer");
 	var shareImage = document.getElementById("shareImage");
-	if (shareImage.hidden === false)
+	if (shareContainer.hidden === false)
 	{
-		shareImage.hidden = true;
-		document.getElementsByClassName("navBtn")[0].innerHTML = "<i class=\"fa fa-share-alt\"></i>&nbsp;分享";
+		shareContainer.hidden = true;
+		document.getElementsByClassName("navBtn")[1].innerHTML = "<i class=\"fa fa-share-alt\"></i>&nbsp;分享";
 		document.getElementById("topListContainer").style.top = "100px";
 		return;
 	}
 	var c = document.getElementById("shareCanvas");
 	var ctx = c.getContext("2d");
-	ctx.clearRect(0,0,400,300);
 	var text = document.getElementById("topList").innerHTML.split("<br>");
 	var x = 10;
-	var y = 20;
+	var y = 60;
 	var fontsize = 14;
-	ctx.font = fontsize + "px 微软雅黑";
 	ctx.fillStyle = "White";
-	ctx.fillRect(0,0,400,300);
+	ctx.fillRect(0,0,400,330);
+	ctx.fillStyle = "#00CDCD";
+	ctx.font = "20px 微软雅黑";
+	var month = new Date().getMonth() + 1;
+	var date = new Date().getDate();
+	ctx.fillText(month + "月" + date + "日" + document.getElementsByClassName("classIndexSelect")[0].value + "排行榜", x, y - 30);
 	ctx.fillStyle = "Black";
+	ctx.font = fontsize + "px 微软雅黑";
 	for (var i = 0; i < text.length / 3; i++)
 	{
 		ctx.fillText(text[i], x, y + i * fontsize + i * 5);
 	}
-	for (var i = parseInt(text.length / 3) + 1; i < text.length / 3 * 2; i++)
+	for (var i = parseInt(text.length / 3); i < text.length / 3 * 2; i++)
 	{
-		ctx.fillText(text[i], x + 125, y + (i - text.length / 3 - 1) * fontsize + (i - text.length / 3) * 5);
+		ctx.fillText(text[i], x + 125, y + (i - text.length / 3) * fontsize + (i - text.length / 3) * 5);
 	}
-	for (var i = parseInt(text.length / 3 * 2) + 1; i < text.length; i++)
+	for (var i = parseInt(text.length / 3 * 2); i < text.length; i++)
 	{
-		ctx.fillText(text[i], x + 250, y + (i - text.length / 3 * 2 - 1) * fontsize + (i - text.length / 3 * 2) * 5);
+		ctx.fillText(text[i], x + 250, y + (i - text.length / 3 * 2) * fontsize + (i - text.length / 3 * 2) * 5);
 	}
+	ctx.fillStyle = "#666";
+	ctx.font = "16px 微软雅黑";
+	ctx.fillText(remark, x, y + 255);
 	shareImage.src = c.toDataURL("image/png");
-	shareImage.hidden = false;
-	document.getElementsByClassName("navBtn")[0].innerHTML = "<i class=\"fa fa-close\"></i>&nbsp;关闭";
-	document.getElementById("topListContainer").style.top = "410px";
+	shareContainer.hidden = false;
+	document.getElementsByClassName("navBtn")[1].innerHTML = "<i class=\"fa fa-close\"></i>&nbsp;关闭";
+	document.getElementById("topListContainer").style.top = "496px";
+	console.log("成功绘制Canvas，转换为Base64并显示");
+}
+//编辑备注
+function editRemark()
+{
+	var remark2 = prompt("修改分享备注（可留空）：");
+	if (remark2 === null)
+	{
+		return false;
+	}
+	remark = remark2;
+	document.getElementById("shareContainer").hidden = true;
+	console.log("成功修改分享备注");
+	share();
 }
 //重置
 function reset()
 {
-	location.reload();
+	for (var i = 0; i < names.length; i++)
+	{
+		document.getElementById(names[i] + "LikeCount").innerHTML = "0";
+	}
+	console.log("成功重置分数");
+	sortTopList();
 }
 
 //神兽保平安 (Consolas字体)-->
