0e3fa4c2b2777cddc9893609e8a55a758d210fde
IcyFlame <2419029544@qq.com>
2018-07-12 18:03:56 +0800

    Update

--- script/script.js
+++ script/script.js
@@ -147,7 +147,7 @@ function checkDebug() {
 }
 
 //Notice Board
-var notice = "";
+var notice = "挖的坑真多，现在正在慢慢填坑";
 if (notice.length === 0) {
 	notice = "这里空空如也";
 }
@@ -211,13 +211,24 @@ if (!debug) {
 var lastScrollY = 0;
 function loadHeaderScroll() {
 	//强行搭趟车（滑稽）
-	if (getBrowser()[0] === "Safari")
+	/*if (getBrowser()[0] === "Safari")
 	{
 		document.getElementsByTagName("header")[0].style.backgroundColor = "rgba(255,255,255,0.6)";
 		document.getElementsByTagName("header")[0].style.backdropFilter = "blur(5px)";
-	}
-	//延时，一开始的动画以及浏览器问题会导致滚动条微微起伏
+	}*/
+	//延时，一开始的动画以及浏览器问题会导致滚动条起伏
 	setTimeout(function() {
+		var header = document.getElementsByTagName("header")[0];
+		if (window.scrollY < 50)
+		{
+			header.style.boxShadow = "0px 0px 0px rgba(0,0,0,0)";
+			header.style.backgroundColor = "rgba(255,255,255,0.8)";
+		}
+		else
+		{
+			header.style.boxShadow = "";
+			header.style.backgroundColor = "";
+		}
 		window.onscroll = function() {
 			headerScroll();
 		};
@@ -231,10 +242,19 @@ function headerScroll() {
 	{
 		bgimg.style.bottom = 50 - window.scrollY / 8 + "px";
 	}*/
+	if (window.scrollY < 50)
+	{
+		header.style.boxShadow = "0px 0px 0px rgba(0,0,0,0)";
+		header.style.backgroundColor = "rgba(255,255,255,0.8)";
+	}
+	else
+	{
+		header.style.boxShadow = "";
+		header.style.backgroundColor = "";
+	}
 	if (window.scrollY - lastScrollY > 0 && window.scrollY > 165)
 	{
 		//往下滚动，隐藏导航栏（加个165表示在页面顶部滚动不隐藏）
-		header.style.transition = "";
 		header.style.top = "-80px";
 	}
 	else if (window.scrollY - lastScrollY < 0)
